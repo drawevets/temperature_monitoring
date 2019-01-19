93fe27fff245e51b3294ee2217721ec28ca5a611
SteveW <fender.sward@gmail.com>
2019-01-19 15:20:35 +0000

    Changed location of code path

--- start_temp_monitoring.sh
+++ start_temp_monitoring.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/steve/Python_Code/temperature_monitoring
+cd /home/steve/temperature_monitoring
 sudo echo "Start Script: About to run - All_Temp_Sensors_To_DB.py" >> log.txt
 sudo python3 all_temps_to_DB.py > /dev/null 2>&1 & 
