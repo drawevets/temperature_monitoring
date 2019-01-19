dc307b29199722e423a775c601f4215b5de195b6
SteveW <fender.sward@gmail.com>
2019-01-19 13:04:57 +0000

    renamed main python monitoring code

--- start_temp_monitoring.sh
+++ start_temp_monitoring.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/steve/Python_Code/temperature_monitoring
-sudo echo "About to run: All_Temp_Sensors_To_DB.py" >> log.txt
-sudo python3 All_Temp_Sensors_To_DB.py > /dev/null 2>&1 & 
+sudo echo "Start Script: About to run - All_Temp_Sensors_To_DB.py" >> log.txt
+sudo python3 all_temp_to_DB.py > /dev/null 2>&1 & 
