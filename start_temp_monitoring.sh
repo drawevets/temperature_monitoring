3a583effdf8ed70d2ab21dc7d43530eddccf22e1
drawevets <46519909+drawevets@users.noreply.github.com>
2019-01-19 13:06:28 +0000

    fixed typo in python code name

--- start_temp_monitoring.sh
+++ start_temp_monitoring.sh
@@ -2,4 +2,4 @@
 
 cd /home/steve/Python_Code/temperature_monitoring
 sudo echo "Start Script: About to run - All_Temp_Sensors_To_DB.py" >> log.txt
-sudo python3 all_temp_to_DB.py > /dev/null 2>&1 & 
+sudo python3 all_temps_to_DB.py > /dev/null 2>&1 & 
