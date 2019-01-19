1011dfc59c4ee7a9f6b7416a4a5223b84bc18d4d
SteveW <fender.sward@gmail.com>
2019-01-19 11:52:08 +0000

    ..

--- start_temp_monitoring.sh
+++ start_temp_monitoring.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/steve/Python_Code/temperature_monitoring
-sudo echo "now starting code" >> log.txt
-sudo python3 All_Temp_Sensors_To_DB.py >> log.txt 2>&1 & 
+sudo echo "About to run: All_Temp_Sensors_To_DB.py" >> log.txt
+sudo python3 All_Temp_Sensors_To_DB.py > /dev/null 2>&1 & 
