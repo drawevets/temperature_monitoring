857ce0442e39ca0a6e3fee61743cec4b1154a9c4
SteveW <fender.sward@gmail.com>
2019-01-21 19:21:29 +0000

    Fetch latest origin master code base from git

---
+++ fetch_origin_master_with_git.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3.5
+
+import sh
+from sh import git
+import time
+import os, sys
+
+def CheckForUpdate(workingDir):
+    print("Fetching most recent code from source..." + workingDir)
+
+    # Fetch most up to date version of code.
+    #p = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "fetch", "origin", "master", _out=ProcessFetch, _out_bufsize=0, _tty_in=True)               
+    p = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "fetch", "origin", "master", _out_bufsize=0, _tty_in=True)               
+    
+    print("Fetch complete.")
+    time.sleep(2)
+    print("Checking status for " + workingDir + "...")
+    statusCheck = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "status")
+
+    if "Your branch is up-to-date" in statusCheck:
+        print("Status check passes.")
+        print("Code up to date.")
+        return False
+    else:
+        print("Code update available.")
+        return True
+
+
+if __name__ == "__main__":
+
+    gitDir = "/home/steve/temperature_monitoring/"
+
+    print("*********** Checking for code update **************")
+    
+    if CheckForUpdate(gitDir):
+        print("Resetting code...")
+        resetCheck = git("--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/master")
+        print(str(resetCheck))
+        print("Check complete")
+
