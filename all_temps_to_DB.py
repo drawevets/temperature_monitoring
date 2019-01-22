02026f63b8baca4903176c648f49b0bdfa71d645
drawevets <46519909+drawevets@users.noreply.github.com>
2019-01-22 13:10:54 +0000

    Added email sending function

--- all_temps_to_DB.py
+++ all_temps_to_DB.py
@@ -8,6 +8,7 @@ import time
 import MySQLdb
 import sys
 import signal
+import smtplib
 import sqlite3
 import subprocess
 import time
@@ -19,6 +20,9 @@ import time
 database_name = "temps"
 database_user_name = "temps_user"
 database_password = "user"
+
+email_user = "raspberrypi.emailsender1972@gmail.com"
+email_passwd = "Raspberry4P!"
 
 Global_db_cursor = None
 Global_db_conn = None
@@ -424,6 +428,26 @@ def get_local_ip_address():
     write_to_log("<< get_local_ip_address()")
     return ip_address
 
+
+def send_email(user, pwd, recipient, subject, body):
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
 
 #############################################################################################################
 
