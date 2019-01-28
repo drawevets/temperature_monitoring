acd593355b906d4d8f540f704b69a3182806b9ec
SteveW <fender.sward@gmail.com>
2019-01-28 18:26:05 +0000

    Fix path to Flask code

--- start_temp_monitoring.sh
+++ start_temp_monitoring.sh
@@ -3,6 +3,5 @@
 cd /home/steve/temperature_monitoring
 sudo echo "Start Script: About to run - All_Temp_Sensors_To_DB.py" >> log.txt
 sudo python3 all_temps_to_DB.py > /dev/null 2>&1 & 
-cd Flask
 sudo python3 flask_demo2.py > /dev/null 2>&1 &
 
