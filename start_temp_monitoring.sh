5f9b5f27ebd4445f95bc988a52842e218bfb2452
SteveW <fender.sward@gmail.com>
2019-02-02 16:47:32 +0000

    more file rename stuff

--- start_temp_monitoring.sh
+++ start_temp_monitoring.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd /home/steve/temperature_monitoring
-sudo echo "Start Script: About to run - All_Temp_Sensors_To_DB.py" >> log.txt
-sudo python3 all_temps_to_DB.py > /dev/null 2>&1 & 
+sudo echo "Start Script: About to run - temps_manage_reading.py" >> log.txt
+sudo python3 temps_manage_reading.py > /dev/null 2>&1 & 
+sudo echo "Start Script: About to run - temps_flask.py" >> log.txt
 sudo python3 temps_flask.py > /dev/null 2>&1 &
 
