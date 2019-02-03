c84a62db728ddc0fbac391d2af1050894fb6de01
SteveW <fender.sward@gmail.com>
2019-02-03 17:49:42 +0000

    Fixed updater at last

--- run_git_fetch.sh
+++ run_git_fetch.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
 start_time=$(date +'%m/%d/%Y %T')
-
 cd /home/steve/temperature_monitoring
-sudo echo "${start_time}: Update with git starting now" >> /home/steve/temperature_monitoring/update_log.txt
-sudo python3 fetch_origin_master_with_git.py >> /home/steve/temperature_monitoring/update_log.txt & 
+echo "${start_time}: Update with git starting now" >> /home/steve/temperature_monitoring/update_log.txt
+/usr/bin/python3 fetch_origin_master_with_git.py >> /home/steve/temperature_monitoring/update_log.txt
 end_time=$(date +'%m/%d/%Y %T')
-sudo echo "${end_time}: Finished update check / complete update" >> /home/steve/temperature_monitoring/update_log.txt
+echo "${end_time}: Finished update check / complete update" >> /home/steve/temperature_monitoring/update_log.txt
 
