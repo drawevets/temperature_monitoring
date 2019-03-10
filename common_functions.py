a3c1f4aef3625f75cd4df27b77a1d1f72aad5039
SteveW <fender.sward@gmail.com>
2019-03-10 16:48:03 +0000

    Added more display options for the line charts

--- common_functions.py
+++ common_functions.py
@@ -20,7 +20,7 @@ base_dir = '/sys/bus/w1/devices/'          # Location of 1 wire devices in the f
 log_to_console = True
 
 def app_version():
-    return ("v0.156 - Last updated: 10/03/19")
+    return ("v0.158 - Last updated: 10/03/19")
 
 
 def check_for_updates(caller):
